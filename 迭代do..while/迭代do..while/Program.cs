5e48ddf05460cbccbc02a381b801e3855a221798
Qidy <tsietsi@live.cn>
2017-05-31 17:05:40 +0800

    do while

--- 迭代do..while/迭代do..while/Program.cs
+++ 迭代do..while/迭代do..while/Program.cs
@@ -10,15 +10,27 @@ namespace 迭代do._.@while
     {
         static void Main(string[] args)
         {
-            int num = 0;
+            double Balance = 0;
+            double Rate = 0;
+            int Year = 0;
+            double TartetBalance = 0;
+            Console.WriteLine("输入您的本金");
+            Balance = double.Parse(Console.ReadLine());
+            Console.WriteLine("输入当前利率百分比");
+            Rate = double.Parse(Console.ReadLine())/100;
+            Console.WriteLine("输入目标收益");
+            TartetBalance = double.Parse(Console.ReadLine());
             do
             {
-                num++;
-                Console.WriteLine(num);
+                Balance *= (Rate + 1);
+                Year++;
 
             }
-            while (num <= 50);
-            Console.ReadKey();
+            while (Balance < TartetBalance);
+            {
+                Console.WriteLine("将在{0}年内，获得{1}元的收益", Year, Balance);
+                Console.ReadKey();
+            }
 
         }
     }
