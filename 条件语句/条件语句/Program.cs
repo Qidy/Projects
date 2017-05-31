3027bbfcb366f3d37fffd43267ea461b87233a88
Qidy <tsietsi@live.cn>
2017-05-31 10:48:01 +0800

    添加If语句

--- 条件语句/条件语句/Program.cs
+++ 条件语句/条件语句/Program.cs
@@ -17,6 +17,20 @@ namespace 条件语句
              * 2.迭代语句
              * 3.跳转语句
              */
+
+            #region if
+            //int roll = int.Parse(Console.ReadLine());
+            Random kk = new Random();
+            int roll = kk.Next(1, 7);
+            if (roll == 1) Console.WriteLine("1");
+            else if (roll == 2) Console.WriteLine("2");
+            else if (roll == 3) Console.WriteLine("3");
+            else if (roll == 4) Console.WriteLine("4");
+            else if (roll == 5) Console.WriteLine("5");
+            else if (roll == 6) Console.WriteLine("6");
+
+
+            #endregion
             #region 声明变量并输入，如果小于10重新输入，输入完成计算
             //i://设置跳转点i
             //    double var1 = Convert.ToDouble(Console.ReadLine());//输入数字
@@ -44,11 +58,11 @@ namespace 条件语句
 
             #region 声明变量并输入，查看与10的大小关系
             //int.Parse将数字的字符串形式转换为它等效32位有符号的整数
-            Console.WriteLine("请输入一个数字，判定与10的关系");
-            int num = int.Parse(Console.ReadLine());
-            if (num > 10) Console.WriteLine("您输入的数值大于10");
-            else if(num ==10) Console.WriteLine("您输入的数值等于10");
-            else Console.WriteLine("您输入的数值小于10");
+            //Console.WriteLine("请输入一个数字，判定与10的关系");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num > 10) Console.WriteLine("您输入的数值大于10");
+            //else if(num ==10) Console.WriteLine("您输入的数值等于10");
+            //else Console.WriteLine("您输入的数值小于10");
 
             #endregion
             Console.ReadKey();
