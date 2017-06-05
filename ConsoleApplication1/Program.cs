d42d4d6561e8b9b339d0181d85d91ac999745e96
Qidy <tsietsi@live.cn>
2017-06-05 11:21:45 +0800

    1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 方法
+{
+    class Program
+    {
+        //方法Maxval(int[] intarray)
+        static int Maxval(int[] intarray)
+        {
+            //声明并初始化为数组第一个值
+            int maxVal = intarray[0];
+            //循环intarray，
+            for (int i = 0; i < intarray.Length; i++)
+            {
+                //如果intarray[i]值大于初始化数组中maxVal值
+                if (intarray[i] > maxVal)
+                {
+                    //将intarray[i]值赋予maxVal
+                    maxVal = intarray[i];
+                }
+            }
+            //返回最大值
+            return maxVal;
+        }
+
+        static double Maxval(double[] intarray)
+        {
+            //声明并初始化为数组第一个值
+            double maxVal = intarray[0];
+            //循环intarray，
+            for (int i = 0; i < intarray.Length; i++)
+            {
+                //如果intarray[i]值大于初始化数组中maxVal值
+                if (intarray[i] > maxVal)
+                {
+                    //将intarray[i]值赋予maxVal
+                    maxVal = intarray[i];
+                }
+            }
+            //返回最大值
+            return maxVal;
+        }
+
+
+        static double getval1(double var1,double var2)
+        {
+            return var1 * var2;
+        }
+        static double getval()
+        {
+            Console.WriteLine("请输入getval值");
+            double a = Convert.ToDouble(Console.ReadLine());
+            if (a < 5)
+            {
+                return 0;
+            }
+            else return a;
+        }
+        static string getstring()
+        {
+            return "hello,world";
+        }
+        static void Main(string[] args)
+        {
+            /*
+             调用方法时候，如有参数传入，必须与方法中参数匹配
+             */
+
+
+            int[] myarray = { 1, 8, 3, 6, 2, 5,15 ,9, 3, 0, 2 };
+            //调用Maxval()方法，把值赋给maxVal
+            int maxVal = Maxval(myarray);
+            Console.WriteLine("数组中最大数是：{0}",maxVal);
+
+            double[] myarray1 = { 1.3, 8.5, 3.6, 6.8, 2.4, 5.1, 15.9, 9.8, 3.4, 2.6 };
+            //调用Maxval()方法，把值赋给maxVal
+            double maxVala = Maxval(myarray1);
+            Console.WriteLine("数组中最大的double是：{0}", maxVala);
+
+            string mystring;
+            //调用getstring()方法
+            mystring = getstring();
+            Console.WriteLine(mystring);
+            double myval;
+            double multipler = 5.3;
+            //调用getval()方法
+            myval = getval() * multipler;
+            Console.Write("getval() * multipler值为：");
+            Console.WriteLine(myval);
+            Console.WriteLine("getval1:"+getval1(3,5));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 引用和值参数/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 引用和值参数
+{
+    class Program
+    {
+
+
+        //关键字params定义方法 SumVals ，该方法可以接受任意个int参数（不接受其他类型参数）
+        static int SumVals(params int[] vals)
+        {
+            int sum = 0;
+            foreach (int item in vals)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        static void Showdouble(ref int Val)
+        {
+            Val *= 2;
+            Console.WriteLine(Val);
+        }
+
+        /*  首先：两者都是按地址传递的，使用后都将改变原来参数的数值。
+            其次：ref可以把参数的数值传递进函数，但是out是要把参数清空，
+            就是说你无法把一个数值从out传递进去的，out进去后，参数的数值为空，
+            所以你必须初始化一次。这个就是两个的区别，或者说就像有的网友说的，
+            ref是有进有出，out是只出不进。
+        */
+
+        static int Showdouble2( int Val)
+        {
+            Val *= 2;
+            return Val;
+        }
+        static void Main(string[] args)
+        {
+            int number = 6;
+            int mynumber = 5;
+            Console.WriteLine(mynumber);
+            Showdouble(ref number);
+            //使用ref后，原值被修改
+            Console.WriteLine(number);
+            mynumber = Showdouble2(mynumber);
+            Console.WriteLine( mynumber);
+            Console.WriteLine(mynumber);
+
+
+
+            int sum = SumVals(1, 5, 2, 9, 8);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
