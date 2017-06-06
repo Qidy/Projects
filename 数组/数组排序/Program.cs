bf1e367424f1ad24b2e8d6c5e46d2c4d9d7ebd30
Qidy <tsietsi@live.cn>
2017-06-06 16:24:10 +0800

    结构调整

---
+++ 数组/数组排序/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 数组排序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myint = { 0, 2, 3, 5, 7, 8, 10, 15, 17, 19, 20, 30, 50, 80, 100, 150 };
+            Array.Sort(myint);
+            Console.WriteLine("Array.Sort():");
+            foreach (int item in myint)
+            {
+                
+                Console.Write(item+ " ");
+            }
+            Console.WriteLine();
+            Array.Reverse(myint);
+            Console.WriteLine("Array.Reverse():");
+            foreach (int outint in myint)
+            {
+               
+                Console.Write(outint+ " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 错误处理/自定义异常类/MyException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace 自定义异常类
+{
+    class MyException:Exception
+    {
+
+       
+
+        //定义异常类型警告信息内容:base(message)
+        public MyException(string message):base(message)
+        {
+            
+        }
+
+       
+    }
+}
